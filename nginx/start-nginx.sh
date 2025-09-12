654b03bd6e61819093de3907f9028c7a2b792a11
pvcordeiro <pv.35@hotmail.com>
2025-09-12 18:02:09 +0100

    changes to use cloudflare

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 set -e
 
-CERT_PATH="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
-KEY_PATH="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
-
-if [ -f "$CERT_PATH" ] && [ -f "$KEY_PATH" ]; then
-  echo "SSL certs found, using HTTPS config"
-  envsubst '$DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-else
-  echo "SSL certs not found, using HTTP-only config"
-  envsubst '$DOMAIN' < /etc/nginx/nginx.conf.http > /etc/nginx/nginx.conf
-fi
+echo "Using Cloudflare SSL configuration"
+envsubst '$DOMAIN' < /etc/nginx/nginx.conf.cloudflare > /etc/nginx/nginx.conf
 
 # Clean up any stale Fail2Ban socket
 rm -f /var/run/fail2ban/fail2ban.sock
